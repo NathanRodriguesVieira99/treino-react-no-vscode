15e4f51a4ea0e062bbd786fa727439db9b3fa33f
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-07-24 17:43:44 -0300

    biome

---
+++ estudos_de_JS.js
@@ -0,0 +1,54 @@
+// OPERADORES
+
+// + soma
+// - subtração
+// * multiplicação
+// / divisão
+//  % módulo
+// ++ incremento
+// -- decremento
+
+ const valor1 =10;
+ const valor2 =2;
+console.log('Soma', valor1 + valor2);
+console.log('Subtração', valor1 - valor2);
+console.log('Multiplicação', valor1 * valor2);
+console.log('Divisaão', valor1 / valor2);
+
+
+//CONDICIONAIS 
+
+// if
+// else
+// switch
+// ternario
+
+
+const idade =25;
+if(idade > 18){
+  console.log('Maior que 18') 
+}else{
+  console.lg('Menor que 18')
+}
+
+const nota = 7
+
+
+
+
+
+// OPERADORES LÓGICOS
+
+// && AND
+// || OR
+// == === IGUAL
+// != ou !=== DIFERENTE
+// > Maior que
+// < Menor que
+
+
+
+
+
+
+
