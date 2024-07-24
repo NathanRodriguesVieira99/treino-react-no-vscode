19eb71a7c4de6dd62ab2dd09206014dc777bfc09
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-07-24 20:54:30 -0300

    parte 1 concluida

--- estudos_de_JS.js
+++ estudos_de_JS.js
@@ -80,23 +80,77 @@ for (let i = 1; i <= 10; i++) {
 		console.log(`${i} x 5 = ${i * 5}`);
 	}
 }
-console.log("======================");
+
 let x = 1;
 while (x < 10) {
 	console.log(`${x} x 5 = ${x * 5}`);
 	x++;
 }
+
 console.log("======================");
 // forEach
-const valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
-// valores.forEach((value) => {
-// 	console.log(`${value} x 5 = ${value * 5}`);
-// });
+// const valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  valores.forEach((value) => {
+//  	console.log(`${value} x 5 = ${value * 5}`);
+//  });
+
+console.log("======================");
+//ARROW FUNCTIONS
+const subtrair = (num1, num2) => num1 - num2;
+const resultadoSub = subtrair(10, 5);
+console.log(resultadoSub);
+
+const somar = (num1, num2) => num1 + num2;
+const resultadoSoma = somar(10, 5);
+console.log(resultadoSoma);
+
+console.log("======================");
+// ARRAYS
+
+//array de string
+const armas = ["glock", "ak-47", "fal", "tec-9"];
+
+//array objeto
+const gunStore = [
+	{ name: "glock-18", brand: "glock", type: 'pistol', price: 100 }, //indice 0
+	{ name: "ak-47", brand: "kalashinikov", type: 'rifle', price: 500 }, //indice 1
+	{ name: "HK-g3", brand: "Heclker & Koch", type: 'pistol', price: 90 }, //indice 2
+	{ name: "P365", brand: "Sig Sauer", type: 'rifle', price: 200 }, //indice 3
+];
+
+//metodo push
+gunStore.push({
+	name: "fal", brand: "FN", type: 'rifle', price: 700 // indice 4
+});
+console.log(gunStore[1].name); // mostra apenas o indice especificado
+
+console.log("======================");
+//metodo filter
+const priceFilter = gunStore.filter(priceGun => priceGun.price >= 90)
+console.table(priceFilter); // filtrou todos os preços maiores que 90
+
+console.log("======================");
+//find
+const fuzisFiltrado = gunStore.find(typeGun => typeGun.type === 'rifle');
+console.table(fuzisFiltrado); // achou todos os que são do tipo rifle
+
+console.log("======================");
+//findIndex
+const fuzisFiltradoIndex = gunStore.findIndex(typeGun => typeGun.type === 'rifle');
+console.table(fuzisFiltradoIndex); // achou todos os indices dos  que são do tipo rifle
+
 console.log("======================");
 //map
 const valores2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const resultado = valores2.map((value) => {
 	return value * 5;
 });
-
 console.log(resultado);
+
+console.log("======================");
+//reduce
+const gunPrice = gunStore.reduce((acc, prices) => {
+	return acc + prices.price;
+}, 0); // calculou todos os preços
+
+console.log(gunPrice)
